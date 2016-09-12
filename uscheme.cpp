8b3b9b659e4b612e49ee8cd70990a11a97d0acc6
Robert Simari <rsimari@roberts-mbp.dhcp.nd.edu>
2016-09-12 19:25:15 -0400

    evaluate_r added

--- uscheme.cpp
+++ uscheme.cpp
@@ -44,23 +44,24 @@ ostream &operator<<(ostream &os, const Node &n) {
 
 string parse_token(istream &s) {
     string token;
-    char next = s.get();
-    if ((next == '(' || next == ')') || (next == '+' || next == '-' || next == '*' || next == '/')) {
+    char next;
+    s >> next;
+    if ((next == '(' || next == ')') || isOperator(string(1, next))) {
         token = next;
     } else if (isdigit(next)) {
         token = next;
         while (isdigit(s.peek())) {
             token += s.get();
         }
-    }
-    cout << token;
+    } 
+    cout << token << endl;
     return token;
 }
 
 Node *parse_expression(istream &s) {
     string currToken = parse_token(s);
-    Node *left;
-    Node *right;
+    Node *left = nullptr;
+    Node *right = nullptr;
     
     if (currToken == "" || currToken == ")") return nullptr;
 
@@ -76,6 +77,18 @@ Node *parse_expression(istream &s) {
 // Interpreter -----------------------------------------------------------------
 
 void evaluate_r(const Node *n, stack<int> &s) {
+    // a and b are on the top of the stack and need to be operated on
+    int a = s.top(); s.pop();
+    int b = s.top(); s.pop();
+    if (n->value == '+') {
+        s.push(a + b);
+    } else if (n->value == '-') {
+        s.push(a - b);
+    } else if (n->value == '*') {
+        s.push(a * b);
+    } else if (n->value == '/') {
+        s.push(a / b);
+    } 
 }
 
 int evaluate(const Node *n) {
